0fc2d65a36e054dec09b676de1a2ffd4fa4b049a
Lucas <lucasdesousa19@hotmail.com>
2020-07-02 20:31:34 -0300

    Update server.js

--- dist/src/server.js
+++ dist/src/server.js
@@ -12,4 +12,4 @@ app.use(cors_1.default());
 app.use(express_1.default.json());
 app.use(routes_1.default);
 app.use('/uploads', express_1.default.static(path_1.default.resolve(__dirname, '..', 'uploads')));
-app.listen(8000);
+app.listen(process.env.PORT || 8000);
